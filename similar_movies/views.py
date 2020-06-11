5ba4e52464a2b3c595165fbad869b580b0168de2
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-11 23:17:49 +0530

    Added JS

--- similar_movies/views.py
+++ similar_movies/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+import imdb
 
 def home(request):
+	# moviesDB = imdb.IMDb()
+	# movies = moviesDB.search_movie('inception')
+	# for movie in movies:
+	# 	title = movie['title']
+	# 	year = movie['year']
+	# 	print(f'{title} :{year}')
+	# id = movies[0].getID()
+	# movie = moviesDB.get_movie(id)
+	# print(movie['title'],movie['year'],movie['cast'],movie['rating'],movie['directors'])
 	return render(request,'similar_movies/index.html')
