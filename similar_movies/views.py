e912a35fed407a8ea1e4caf7fb7fc5073ceb6922
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-13 19:49:20 +0530

    Added tmdb api

--- similar_movies/views.py
+++ similar_movies/views.py
@@ -1,42 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from imdb import IMDb, IMDbError
+import tmdbsimple as tmdb
 
 moviesDB = IMDb()
+tmdb.API_KEY = 'a25592d6509c51f48ff31ed09207fbaf'
+
 def home(request):
-	# moviesDB = imdb.IMDb()
-	# movies = moviesDB.search_movie('inception')
-	# for movie in movies:
-	# 	title = movie['title']
-	# 	year = movie['year']
-	# 	print(f'{title} :{year}')
-	# id = movies[0].getID()
-	# movie = moviesDB.get_movie(id)
-	# print(movie['title'],movie['year'],movie['cast'],movie['rating'],movie['directors'])
 	return render(request,'similar_movies/index.html')
 
 def search_movie(request):
 	if request.method == 'POST':
-		movie_name = request.POST['movie-name']
 		try:
-			movies = moviesDB.search_movie(movie_name)
-			movieList = []
-			for movie in movies:
-				id = movie.movieID
-				current_movie = moviesDB.get_movie(id)
-
-				title = current_movie['title']
-				cast = ', '.join(map(str,current_movie['cast']))
-				directors = ', '.join(map(str,current_movie['directors']))
-				year = current_movie['year']
-				plot = current_movie['plot']
-				rating = current_movie['rating']
-				# movieList.append([title,year,rating,cast,directors])
-			# print(movieList)
-
-		except IMDbError as e:
-			return JsonResponse({'error':e},safe=False)	
-
-		return JsonResponse({'data':'here'},safe=False)
+			movie_name = request.POST['movie-name']
+			search  = tmdb.Search()
+			response = search.movie(query=movie_name)
+			return JsonResponse({'data':search.results},safe=False)
+		except :
+			return JsonResponse({'error':"Error while fetching movie"},safe=False)
 	else:
 		return JsonResponse({'error':'Only POST requests allowed'},safe=False)
