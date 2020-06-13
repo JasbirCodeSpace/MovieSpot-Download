2b68705bf3e2ad0fdc56b702e02cbb55c28a24ae
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-13 21:21:00 +0530

    Other Changes

--- similar_movies/urls.py
+++ similar_movies/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('',views.home,name='movie-home'),
-	path('search-movie',views.search_movie,name='search-movie')
+	path('search-movie',views.search_movie,name='search-movie'),
+	path('get-genre',views.get_genre,name='get-genre')
 ]
--- similar_movies/views.py
+++ similar_movies/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from imdb import IMDb, IMDbError
 import tmdbsimple as tmdb
+import requests
 
 moviesDB = IMDb()
 tmdb.API_KEY = 'a25592d6509c51f48ff31ed09207fbaf'
@@ -9,12 +10,16 @@ tmdb.API_KEY = 'a25592d6509c51f48ff31ed09207fbaf'
 def home(request):
 	return render(request,'similar_movies/index.html')
 
+def get_genre(request):
+	response = requests.get('https://api.themoviedb.org/3/genre/movie/list?api_key=' +  tmdb.API_KEY + '&language=en-US')
+	return JsonResponse(response.json(),safe=False)
 def search_movie(request):
 	if request.method == 'POST':
 		try:
 			movie_name = request.POST['movie-name']
 			search  = tmdb.Search()
 			response = search.movie(query=movie_name)
+			# print(search.genre())
 			return JsonResponse({'data':search.results},safe=False)
 		except :
 			return JsonResponse({'error':"Error while fetching movie"},safe=False)
