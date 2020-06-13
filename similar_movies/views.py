871a6750c381be9cf54142f6d9395d117658270f
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-13 14:31:39 +0530

    Updated views

--- similar_movies/views.py
+++ similar_movies/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-import imdb
+from django.http import HttpResponse,JsonResponse
+from imdb import IMDb, IMDbError
 
+moviesDB = IMDb()
 def home(request):
 	# moviesDB = imdb.IMDb()
 	# movies = moviesDB.search_movie('inception')
@@ -12,3 +14,29 @@ def home(request):
 	# movie = moviesDB.get_movie(id)
 	# print(movie['title'],movie['year'],movie['cast'],movie['rating'],movie['directors'])
 	return render(request,'similar_movies/index.html')
+
+def search_movie(request):
+	if request.method == 'POST':
+		movie_name = request.POST['movie-name']
+		try:
+			movies = moviesDB.search_movie(movie_name)
+			movieList = []
+			for movie in movies:
+				id = movie.movieID
+				current_movie = moviesDB.get_movie(id)
+
+				title = current_movie['title']
+				cast = ', '.join(map(str,current_movie['cast']))
+				directors = ', '.join(map(str,current_movie['directors']))
+				year = current_movie['year']
+				plot = current_movie['plot']
+				rating = current_movie['rating']
+				# movieList.append([title,year,rating,cast,directors])
+			# print(movieList)
+
+		except IMDbError as e:
+			return JsonResponse({'error':e},safe=False)	
+
+		return JsonResponse({'data':'here'},safe=False)
+	else:
+		return JsonResponse({'error':'Only POST requests allowed'},safe=False)
