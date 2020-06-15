35c0bcd4e585244bbbad011bb09a63580ca0ff92
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-15 17:23:03 +0530

    Other changes

--- similar_movies/urls.py
+++ similar_movies/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	path('',views.home,name='movie-home'),
 	path('search-movie',views.search_movie,name='search-movie'),
 	path('get-genre',views.get_genre,name='get-genre'),
-	path('browse-movie',views.browse_movie,name='browse-movie')
+	path('browse-movie',views.browse_movie,name='browse-movie'),
+	path('browse-movie-form',views.browse_movie_form,name='browse-movie-form')
 ]
