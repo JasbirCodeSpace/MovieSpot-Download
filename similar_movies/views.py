4fbf61923b080728948c07e15f5172379083f8dc
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-15 17:22:35 +0530

    Added method for browse movies

--- similar_movies/views.py
+++ similar_movies/views.py
@@ -3,15 +3,37 @@ from django.http import HttpResponse,JsonResponse
 from imdb import IMDb, IMDbError
 import tmdbsimple as tmdb
 import requests
+from urllib import parse
 
 moviesDB = IMDb()
 tmdb.API_KEY = 'a25592d6509c51f48ff31ed09207fbaf'
 
 def home(request):
 	return render(request,'similar_movies/index.html')
+
 def browse_movie(request):
 	return render(request,'similar_movies/browse.html')
 
+def browse_movie_form(request):
+	if request.method == 'POST':
+
+		query_term = request.POST.get('movie-name')
+		quality = request.POST.get('movie-quality')
+		genre = request.POST.get('movie-genre')
+		sort_by = request.POST.get('movie-order')
+		year = request.POST.get('movie-year')
+		language = request.POST.get('movie-language')
+
+		required_dict = {'query_term':query_term,'quality':quality,'genre':genre,'sort_by':sort_by,'limit':50}
+		response = requests.get('https://yts.mx/api/v2/list_movies.json',params=required_dict)
+		if response:
+			return JsonResponse({'status':True,'data':response.json()},safe=False)
+		else :
+			return JsonResponse({'status':False,'error':response.json()},safe=False)
+
+	return JsonResponse({'status':False,'error':'Invalid Request'},safe=False)
+
+	
 def get_genre(request):
 	response = requests.get('https://api.themoviedb.org/3/genre/movie/list?api_key=' +  tmdb.API_KEY + '&language=en-US')
 	return JsonResponse(response.json(),safe=False)
