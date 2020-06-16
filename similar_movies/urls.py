e672c90825049e4d92372a2cd2bd943c10f37374
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-16 15:33:12 +0530

    Updated url patterns

--- similar_movies/urls.py
+++ similar_movies/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+import django.views.defaults as default_view
 from . import views
 
 urlpatterns = [
@@ -6,5 +7,6 @@ urlpatterns = [
 	path('search-movie',views.search_movie,name='search-movie'),
 	path('get-genre',views.get_genre,name='get-genre'),
 	path('browse-movie',views.browse_movie,name='browse-movie'),
-	path('browse-movie-form',views.browse_movie_form,name='browse-movie-form')
+	path('browse-movie-form',views.browse_movie_form,name='browse-movie-form'),
+	path('movie/<str:movie_id>',views.movie,name='movie'),
 ]
