5fcbf574f2bf3e82b41a152890e61ac8644d914f
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-22 14:49:04 +0530

    allowed host

--- MovieSpot/settings.py
+++ MovieSpot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ['DEBUG_VALUE'] == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moviespotapp.herokuapp.com']
 
 
 # Application definition
