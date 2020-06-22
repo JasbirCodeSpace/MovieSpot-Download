0e15e64de04748df1c6820068783fa04bd075d7b
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-22 14:34:10 +0530

    staticfiles dir for heroku

--- MovieSpot/settings.py
+++ MovieSpot/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
+
 
 django_heroku.settings(locals())
